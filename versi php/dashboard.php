615435ed087534ccdea333b8ee0bc7b5db79157d
FroniWahyudi <froniwahyudi@gmail.com>
2025-05-07 09:15:55 +0700

    hapus file package

--- versi php/dashboard.php
+++ versi php/dashboard.php
@@ -52,7 +52,8 @@ function excerpt(string $text, int $maxLen = 100): string {
   <title>Dashboard - Naga Hytam Sejahtera Abadi</title>
   <link rel="stylesheet" href="bootstrap-5.3.5-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="styles.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"> -->
+  <link rel="stylesheet" href="package/font/bootstrap-icons.css">
   <style>
   /* === GAYA NAVBAR UTAMA === */
   .navbar-custom {
